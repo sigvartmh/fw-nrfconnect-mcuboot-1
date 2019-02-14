1645611565bda9c8a41fd5693549e27941f605f2
Sigvart Hovland <sigvart.m@gmail.com>
2019-02-14 13:20:34 +0100

    [nrf noup] boot: bootutil: Add shared crypto for ECDSA and SHA
    * Add functions for ecdsa_verify_secp256r1 and sha256 to use the shared
    crypto API
    * Add Kconfig and CMake variables for selecting shared crypto when using
    ecdsa
    * Add custom section to project for placing the API section in the
    correct location in flash
    
    Signed-off-by: Sigvart Hovland <sigvart.m@gmail.com>
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    (cherry picked from commit fd12a3d0691e0b58eb2e7835c7b8e4cdc57a911d)
    Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

--- boot/bootutil/include/bootutil/crypto/ecdsa_p256.h
+++ boot/bootutil/include/bootutil/crypto/ecdsa_p256.h
@@ -14,6 +14,7 @@
 
 #if (defined(MCUBOOT_USE_TINYCRYPT) + \
      defined(MCUBOOT_USE_CC310) + \
+     defined(MCUBOOT_USE_NRF_EXTERNAL_CRYPTO) + \
      defined(MCUBOOT_USE_MBED_TLS)) != 1
     #error "One crypto backend must be defined: either CC310, TINYCRYPT, or MBED_TLS"
 #endif
@@ -33,6 +34,11 @@
     #include <mbedtls/ecdsa.h>
     #define BOOTUTIL_CRYPTO_ECDSA_P256_HASH_SIZE (4 * 8)
 #endif
+
+#if defined(MCUBOOT_USE_NRF_EXTERNAL_CRYPTO)
+    #include <bl_crypto.h>
+    #define BOOTUTIL_CRYPTO_ECDSA_P256_HASH_SIZE (4 * 8)
+#endif /* MCUBOOT_USE_NRF_EXTERNAL_CRYPTO */
 
 #ifdef __cplusplus
 extern "C" {
@@ -156,6 +162,27 @@ static inline int bootutil_ecdsa_p256_verify(bootutil_ecdsa_p256_context *ctx,
     return 0;
 }
 #endif /* MCUBOOT_USE_MBED_TLS */
+
+#if defined(MCUBOOT_USE_NRF_EXTERNAL_CRYPTO)
+typedef uintptr_t bootutil_ecdsa_p256_context;
+
+static inline void bootutil_ecdsa_p256_init(bootutil_ecdsa_p256_context *ctx)
+{
+    (void)ctx;
+}
+
+static inline void bootutil_ecdsa_p256_drop(bootutil_ecdsa_p256_context *ctx)
+{
+    (void)ctx;
+}
+
+static inline int bootutil_ecdsa_p256_verify(bootutil_ecdsa_p256_context *ctx, uint8_t *pk, uint8_t *hash, uint8_t *sig)
+{
+    (void)ctx;
+    return bl_secp256r1_validate(hash, BOOTUTIL_CRYPTO_ECDSA_P256_HASH_SIZE,
+                                 pk, sig);
+}
+#endif /* MCUBOOT_USE_NRF_EXTERNAL_CRYPTO */
 
 #ifdef __cplusplus
 }
